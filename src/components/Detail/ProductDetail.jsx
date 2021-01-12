27cc4fc565e98e3b052322edbbb64e7748e6c4ae
Karina Mastbaum <karina.mastbaum@gmail.com>
2021-01-12 15:10:20 -0300

    "fixes"

--- src/components/Detail/ProductDetail.jsx
+++ src/components/Detail/ProductDetail.jsx
@@ -1,19 +1,44 @@
+import {useState, useContext} from 'react';
 import './ProductDetail.css';
+// import {Store} from '../../store';
+import {useHistory} from 'react-router-dom';
 
 const ProductDetail = ({item}) => {
+    const history = useHistory();
+    // const [data, setData] = useContext(Store);
+    const [qty, setQty] = useState(1);	
 
-    const handleClickAdd = (e) => {
-        alert('Producto agregado al carrito');
+    const handleClickResta = () => {	
+        if(qty > 1) {	
+            setQty(qty - 1);	
+        }	
+    }	
+
+    const onAdd = () => {
+
+        history.push('/cart');
+	
     }
-
+    
     return (
         <article>
             <div className="container">
             <h1 className="titulo">{item.titulo}</h1>
-                <img  className="imagen" src={item.imagen} alt="dress1"/>
+                <img  className="imagen" src={item.imagen} alt="dress"/>
                     <p className="descripcion">{item.descripcion}</p>
                     <p className="precio">${item.precio}</p>
-                    <button className="botonProductItem"  onClick={handleClickAdd}>Agregar al Carrito</button>
+
+                    <div className="qty">	
+                    <button 	
+                        disabled={qty === 1 ? 'disabled' : null } 	
+                        onClick={handleClickResta}	
+                    />	
+                
+                    <button className="botonProductItem" onClick={onAdd}>Agregar al Carrito</button>
+
+                    <input type="text" value={qty} readOnly/>	
+                    <button onClick={() => setQty(qty + 1)}>+</button>
+                    </div>
                 </div>
         </article> 
     )
--- src/components/Detail/index.jsx
+++ src/components/Detail/index.jsx
@@ -1,65 +1,78 @@
 import {useEffect, useState} from 'react';
 import ProductDetail from './ProductDetail';
-import {useParams, Link} from 'react-router-dom';
-import {products} from '../products';
-import dress1 from '../../images/dress1.jpg';
-import dress2 from '../../images/dress2.jpg';
-import dress3 from '../../images/dress3.jpg';
-import dress4 from '../../images/dress4.jpg';
-
+import {useParams} from 'react-router-dom';
+// import ProductDetail from "./ProductDetail";
+// import {products} from '../products';
+import dress1 from "../../images/dress1.jpg";
+import dress2 from "../../images/dress2.jpg";
+import dress3 from "../../images/dress3.jpg";
+import dress4 from "../../images/dress4.jpg";
 
 const ItemDetail = ({}) => {
    const {id} = useParams();
    const [product, setProduct] = useState(null);
 
-   const getProduct = new Promise ((resolve) => {
-      const selectedProduct = products.filter(item => item.id);
-        resolve(selectedProduct[0]);
-      });
-//       setTimeout(() => {
-//           resolve({
-//              id: 1,
-//             titulo: "Vestido Naomi floral",
-//             imagen: dress1,
-//             descripcion: "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo, super comodo, perfecto para dia como para noche",
-//             precio: 2500,
-//           });
-//       }, 2000)
-//   })
-
-
-  useEffect (() => {
-
-   getProduct.then( response => setProduct(response));
-
-  }, [])
+   const getProduct = (id) => {
+   const items = [
+   {
+      id: 1,
+      titulo: "Vestido Naomi floral",
+      imagen: dress1,
+      descripcion:
+        "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
+      precio: 2500,
+    },
+    {
+      id: 2,
+      titulo: "Vestido Lola fruncido",
+      imagen: dress2,
+      descripcion: "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
+      precio: 3000,
+    },
+    {
+      id: 3,
+      titulo: "Vestido Puket floral",
+      imagen: dress3,
+      descripcion: "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
+      precio: 3500,
+    },
+    {
+      id: 4,
+      titulo: "Vestido Camille con lunares",
+      imagen: dress4,
+      descripcion: "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
+      precio: 4000,
+    },
+  ];
+  
+  return new Promise((resolve) => {
+   setTimeout(() => {
+     resolve(items.filter(i => i.id === Number(id)))
+   }, 2000);
+ });
+}
+useEffect(() => {
+   console.log(`El id es ${id}`)
+  getProduct(id)
+    .then((response) => {
+      setProduct(response[0]);
+      console.log(response, "detail");
+    })
+    .catch((err) => console.log(err));
+}, [id]);
 
 
 return (
-   <>
-      {
-         product ?
-            <div classNAme="container">
-               <ol className="breadcrum">
-                  <li>
-                        <Link to={`/${product.category}`}>{product.category.split('-').join(' ')}</Link>
-                  </li>
-                  <li>
-                        {product.title}
-                  </li>
-               </ol>
-                    
-      
-               <ProductDetail item={product}/> 
-
-            </div> :
-             
-         <p>Cargando Producto...</p>
-             
-      }
-
-     </>
-)
+      <>
+        {product ? (
+          <div className="container">
+            <ProductDetail item={product} />
+          </div>
+        ) : (
+          <p>Cargando Producto...</p>
+        )}
+      </>
+    );
 
 }
 
