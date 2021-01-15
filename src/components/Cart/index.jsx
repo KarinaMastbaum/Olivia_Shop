48dfb2c330764575ce12da24b6ae1e47d75c1c4f
Karina Mastbaum <karina.mastbaum@gmail.com>
2021-01-14 23:11:40 -0300

    fixes

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,16 +1,36 @@
-import {useContext} from 'react';
+import {useContext, Link} from 'react';
 import {Store} from '../../store';
 
 const Cart = () => {
     const [data, setData] = useContext(Store);
 
+    console.log(data);
+
     return (
         <>
-            <h1>Estás en el cart</h1>
+            <h1>Carrito</h1>
 
             {
-                data.items.map(item => <h2>{item.title}</h2>)
+                data.items.map(item => (
+                    <li>
+                        <div>
+                            <h2>{item.title}</h2>
+                            <p>Cantidad: {item.cantidad}</p>
+                            <p>Precio por unidad: <strong>${item.item.price}</strong></p>
+                            <p>Precio total: <strong>${item.item.price * item.cantidad}</strong></p>
+                        </div>
+                    </li>
+
+                ))
             }
+
+            
+            <div className="final">
+                <p>Precio total: {data.Total}</p>
+
+                <Link to="/checkout">Finalizar compra</Link>
+            </div>
+               
         </>
     )
 }
