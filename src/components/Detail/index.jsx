7eeb90bab93b2314dc19040b5dceaf3932dd8d38
Karina Mastbaum <karina.mastbaum@gmail.com>
2021-01-19 23:48:57 -0300

    add firebase

--- src/components/Detail/index.jsx
+++ src/components/Detail/index.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import ProductDetail from './ProductDetail';
 import {useParams} from 'react-router-dom';
+import {getFirestore} from '../../db';
 import dress1 from '../../images/dress1.jpg';
 import dress2 from '../../images/dress2.jpg';
 import dress3 from '../../images/dress3.jpg';
@@ -13,59 +14,54 @@ const ItemDetail = () => {
   const [product, setProduct] = useState(null);
 
   const { id } = useParams();
+  const db = getFirestore();
 
-  const getProduct = (id) => {
-   const items = [
-      {
-        id: 1,
-        titulo: "Vestido Naomi floral",
-        imagen: dress1,
-        descripcion:
-          "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
-        precio: 2500,
-      },
-      {
-        id: 2,
-        titulo: "Vestido Lola fruncido",
-        imagen: dress2,
-        descripcion:
-          "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
-        precio: 3000,
-      },
-      {
-        id: 3,
-        titulo: "Vestido Puket floral",
-        imagen: dress3,
-        descripcion:
-          "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
-        precio: 3500,
-      },
-      {
-        id: 4,
-        titulo: "Vestido Camille con lunares",
-        imagen: dress4,
-        descripcion:
-          "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
-        precio: 4000,
-      },
-    ];
-     return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(items.filter(i => i.id === Number(id)))
-      }, 2000);
-    });
-  } 
+  // const getProduct = (id) => {
+  //  const items = [
+  //     {
+  //       id: 1,
+  //       titulo: "Vestido Naomi floral",
+  //       imagen: dress1,
+  //       descripcion:
+  //         "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
+  //       precio: 2500,
+  //     },
+  //     {
+  //       id: 2,
+  //       titulo: "Vestido Lola fruncido",
+  //       imagen: dress2,
+  //       descripcion:
+  //         "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
+  //       precio: 3000,
+  //     },
+  //     {
+  //       id: 3,
+  //       titulo: "Vestido Puket floral",
+  //       imagen: dress3,
+  //       descripcion:
+  //         "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
+  //       precio: 3500,
+  //     },
+  //     {
+  //       id: 4,
+  //       titulo: "Vestido Camille con lunares",
+  //       imagen: dress4,
+  //       descripcion:
+  //         "Ultima tendencia, no importa quién seas, de dónde seas y qué estés haciendo",
+  //       precio: 4000,
+  //     },
+  //   ];
+  // } 
 
   useEffect(() => {
-     console.log(`El id es ${id}`)
-    getProduct(id)
-      .then((response) => {
-        setProduct(response[0]);
-        console.log(response, "detail");
-      })
-      .catch((err) => console.log(err));
-
-  }, [id]);
+    db.collection('productos').doc(id).get()
+    .then(doc => {
+        if(doc.exists) {
+            setProduct({id: doc.id, data: doc.data()});
+        }
+    })
+    .catch(e => console.log(e));
+  }, []);
 
   return (
     <>
